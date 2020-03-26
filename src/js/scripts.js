20bc3a7191cfe6136ec4cf57b0752487cac100c6
jasbel <asbel.elmec@gmail.com>
2020-03-26 09:57:24 -0400

    importando todo

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,7 +2,26 @@
   window.sr = ScrollReveal();
   sr.reveal("header", {
     duration: 2000,
+    reset: true
+  });
+  sr.reveal("img", {
+    duration: 800,
+    reset: true
+  });
+  sr.reveal("p", {
+    duration: 1500,
+    reset: true
+  });
+  sr.reveal("h2", {
+    duration: 1500,
     origin: "bottom",
+    distance: "30px",
+    reset: true
+  });
+  sr.reveal("a", {
+    duration: 1500,
+    origin: "top",
+    distance: "30px",
     reset: true
   });
 })();
@@ -20,5 +39,31 @@ $(document).ready(function() {
     // slidesToScroll: 1,
     // autoplay: true,
     // autoplaySpeed: 3000
+  });
+  $(".fads-slider").slick({
+    prevArrow: false,
+    nextArrow: false,
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    fade: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
+  $(".fads-play-slider").slick({
+    prevArrow: false,
+    nextArrow: false,
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    fade: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
   });
 });
